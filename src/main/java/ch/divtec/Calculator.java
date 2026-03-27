8f7ede796aa6d9fec99bd38270a2f57c8daf118c
Nolan Jodry <nolan.jodry@divtec.ch>
2026-03-27 11:46:50 +0100

    ajout test factoriel et autre

--- src/main/java/ch/divtec/Calculator.java
+++ src/main/java/ch/divtec/Calculator.java
@@ -1,24 +1,92 @@
 package ch.divtec;
 
+import java.util.Scanner;
+
 public class Calculator {
 
-    public int add(int a, int b) {
+//    public int add(int a, int b) {
+//        return a + b;
+//    }
+//
+//    public int subtract(int a, int b) {
+//        return a - b;
+//    }
+//
+//    public int multiply(int a, int b) {
+//        return a * b;
+//    }
+//
+//    public int divide(int a, int b) {
+//        return a / b;
+//    }
+
+    double add(double a, double b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    double subtract(double a, double b) {
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    double multiply(double a, double b) {
         return a * b;
     }
 
-    public int divide(int a, int b) {
+    double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division par zéro impossible.");
+        }
         return a / b;
     }
 
+    long factorial(double n) {
+        if (n != (int)n) {
+            throw new IllegalArgumentException("Le factoriel est défini uniquement pour les entiers.");
+        }
+
+        long fact = 1;
+        for (int i = 1; i <= (int) n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        double num1, num2 = 0.;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Allowed operators: + - * / !");
+        System.out.println("Enter the operation to compute :");
+
+        num1 = sc.nextDouble();
+        char operator = sc.next().charAt(0);
+        if (operator != '!') {
+            num2 = sc.nextDouble();
+        }
+
+        final Calculator calc = new Calculator();
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = calc.add(num1, num2);
+                break;
+            case '-':
+                result = calc.subtract(num1, num2);
+                break;
+            case '*':
+                result = calc.multiply(num1, num2);
+                break;
+            case '/':
+                result = calc.divide(num1, num2);
+                break;
+            case '!':
+                result = calc.factorial(num1);
+                break;
+            default:
+                System.out.println("Invalid operator.");
+                return;
+        }
+        System.out.println("The final result: " + num1 + " " + operator + " " + num2 + " = " + result);
     }
 }
--- src/test/java/ch/divtec/CalculatorTest.java
+++ src/test/java/ch/divtec/CalculatorTest.java
@@ -9,107 +9,116 @@ class CalculatorTest {
     @Test
     void add() {
         Calculator calculator = new Calculator();
-        int result = calculator.add(1, 2);
-        assertEquals(3, result);
+        assertEquals(3, calculator.add(1.0, 2.0));
     }
     @Test
     void addNegative() {
         Calculator calculator = new Calculator();
-        int result = calculator.add(-1, 2);
+        assertEquals(1,calculator.add(-1.0, 2.0));
     }
     @Test
     void addZero() {
         Calculator calculator = new Calculator();
-        int result = calculator.add(0, 0);
-        assertEquals(0, result);
+        assertEquals(0, calculator.add(0, 0));
     }
     @Test
     void addMax() {
         Calculator calculator = new Calculator();
-        int result = calculator.add(150000, 50000);
-        assertEquals(200000, result);
+        assertEquals(200000, calculator.add(150000, 50000));
     }
 
     @Test
     void subtract() {
         Calculator calculator = new Calculator();
-        int result = calculator.subtract(2, 1);
-        assertEquals(1, result);
+        assertEquals(1, calculator.subtract(2, 1));
     }
     @Test
     void subtractNegative() {
         Calculator calculator = new Calculator();
-        int result = calculator.subtract(-1, 2);
-        assertEquals(-3, result);
+        assertEquals(-3, calculator.subtract(-1, 2));
     }
 
     @Test
     void subtractZero() {
         Calculator calculator = new Calculator();
-        int result = calculator.subtract(2, 0);
-        assertEquals(2, result);
+        assertEquals(2, calculator.subtract(2, 0));
     }
 
     @Test
     void subtractMax() {
         Calculator calculator = new Calculator();
-        int result = calculator.subtract(100000, 50000);
-        assertEquals(50000, result);
+        assertEquals(50000, calculator.subtract(100000, 50000));
     }
 
     @Test
     void multiply() {
         Calculator calculator = new Calculator();
-        int result = calculator.multiply(1, 2);
-        assertEquals(2, result);
+        assertEquals(2, calculator.multiply(1, 2));
     }
 
     @Test
     void multiplyNegative() {
         Calculator calculator = new Calculator();
-        int result = calculator.multiply(-1, 2);
-        assertEquals(-2, result);
+        assertEquals(-2, calculator.multiply(-1, 2));
     }
 
     @Test
     void multiplyZero() {
         Calculator calculator = new Calculator();
-        int result = calculator.multiply(0, 2);
-        assertEquals(0, result);
+        assertEquals(0, calculator.multiply(0, 2));
+    }
+
+    @Test
+    void multiplyFirstZero() {
+        Calculator calculator = new Calculator();
+        assertEquals(0, Math.abs(calculator.multiply(0.0, -12345.0)));
     }
 
     @Test
     void multiplyMax() {
         Calculator calculator = new Calculator();
-        int result = calculator.multiply(100, 500);
-        assertEquals(50000, result);
+        assertEquals(50000, calculator.multiply(100, 500));
     }
 
     @Test
     void divide() {
         Calculator calculator = new Calculator();
-        int result = calculator.divide(1, 2);
-        assertEquals(0, result);
+        assertEquals(0.5, calculator.divide(1, 2));
     }
 
     @Test
     void divideNegative() {
         Calculator calculator = new Calculator();
-        int result = calculator.divide(-1, 2);
-        assertEquals(0, result);
+        assertEquals(-0.5, calculator.divide(-1, 2));
     }
 
     @Test
     void divideZero() {
         Calculator calculator = new Calculator();
-        int result = calculator.divide(0, 2);
-        assertEquals(0, result);
+        assertThrows(ArithmeticException.class, () -> calculator.divide(0, 0));
     }
 
     @Test
     void divideMax() {
         Calculator calculator = new Calculator();
-        int result = calculator.divide(100000, 50000);
-        assertEquals(2, result);
+        assertEquals(2, calculator.divide(100000, 50000));
+    }
+
+    @Test
+    void factorial() {
+        Calculator calculator = new Calculator();
+        long result = calculator.factorial(5.0);
+        assertEquals(120, result);
+    }
+
+    @Test
+    void factorialNotInteger() {
+        Calculator calculator = new Calculator();
+
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> calculator.factorial(5.2));
+
+        assertEquals("Le factoriel est défini uniquement pour les entiers.",
+                exception.getMessage());
     }
 }
